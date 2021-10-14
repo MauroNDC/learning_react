0cc847c12084c13ff3c8ff0793145a055e751296
MauroDC <mauro.ndicesare@gmail.com>
2021-10-14 20:10:20 -0300

    Detalles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,20 +6,20 @@ const NavBar = () => {
     <>
       <header>
         <nav className="navbar navbar-expand-lg navbar-dark">
-          <a className="navbar-brand" href="#"><span><h4>JewelTools</h4></span><img className="icon-img" src="IMG/logo.png" alt="" /></a>
+          <a className="navbar-brand" href="/"><span><h4>JewelTools</h4></span><img className="icon-img" src="IMG/logo.png" alt="" /></a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarText">
                 <ul className="navbar-nav mr-auto">
                   <li className="nav-item active ml-8">
-                    <a className="nav-link" href="#">Inicio</a>
+                    <a className="nav-link" href="/">Inicio</a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="#">Compras</a>
+                    <a className="nav-link" href="/">Compras</a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="#">Contacto</a>
+                    <a className="nav-link" href="/">Contacto</a>
                   </li>
                 </ul>
                 <CartWidget/>
