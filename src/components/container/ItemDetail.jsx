23bff1058ff723dcbd1ece23c786be6ed236cf87
MauroDC <mauro.ndicesare@gmail.com>
2021-11-06 23:55:32 -0300

    desafio_clase4 Incompleta

--- src/components/container/ItemDetail.jsx
+++ src/components/container/ItemDetail.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-const ItemDetail = ({items}) => 
+const ItemDetail = ({pictureUrl, title, description, price}) => 
     <section className="mb-5">
        <div className="card mb-3" style={{maxWidth: '1480px'}}>
             <div className="row g-0">
                 <div className="col-md-4">
-                    <img src={items.pictureUrl} className="img-fluid rounded-start" alt="..." />
+                    <img src={pictureUrl} className="img-fluid rounded-start" alt="..." />
                 </div>
                 <div className="col-md-8">
                     <div className="card-body">
-                        <h5 className="card-title">{items.title}</h5>
-                        <p className="card-text">{items.description}</p>
-                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <p><strong>${price}</strong></p>
                     </div>
                 </div>
             </div>
--- src/components/container/ItemDetailContainer.jsx
+++ src/components/container/ItemDetailContainer.jsx
@@ -3,32 +3,32 @@ import ItemDetail from './ItemDetail';
 import {useParams} from 'react-router-dom';
 
 const ItemDetailContainer = () => {
-    const [items, setItems] = useState({});
+    const [item, setItem] = useState({});
 
     const {itemId} = useParams();
 
     useEffect(() => {
-        const getItems = () => new Promise (resolve => {
+        const getItems =  new Promise (resolve => {
             setTimeout(() => resolve(
                 [
-                    {id:0, title:'Pinza', description: 'Es una pinza', price: 400, pictureUrl: 'IMG/pinza.jpg'},
+                    {id:0, title:'Pinza', description: 'Es una pinza', price: 400, pictureUrl: 'public/IMG/pinza.jpg'},
                     {id:1, title:'Alicate', description: 'Es una alicate', price: 350, pictureUrl: 'IMG/alicate.jpg'},
                     {id:2, title:'Martillo', description: 'Es una martillo', price: 450, pictureUrl: 'IMG/martillo.jpg'},
                     {id:3, title:'Destornillador', description: 'Es una destornillador', price: 300, pictureUrl: 'IMG/destornillador.jpg'},
                     {id:4, title:'Taladro', description: 'Es un taladro', price: 10000, pictureUrl: 'IMG/taladro.jpg'},
                     {id:5, title:'Moladora', description: 'Es una moladora', price: 12000, pictureUrl: 'IMG/moladora.jpg'}
                 ]
-            ), 200);
+            ), 2000);
         });
 
-        getItems().then((res) => {
-            itemId ? setItems(res.find((it) => it.id === itemId)) : setItems(res)
-        .catch(res => {alert('Error al tratar de renderizar los productos')})
+        getItems.then((res) => {
+            setItem(res.find(it => (it.id = itemId)));
+        //.catch(res => {alert('Error al tratar de renderizar los productos')})
         });
     }, [itemId]);
 
     return(
-        <ItemDetail items={items} />
+        <ItemDetail {...item} />
     )
 }
 
